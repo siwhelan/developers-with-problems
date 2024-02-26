6a1ce80861ed488ca4fbec34179fd507d9620ac1
Sophie Earl <sophie.earl@live.com>
2024-02-26 16:13:29 +0000

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="bg-red-800 font-bold underline">Welcome to SvelteKit</h1>
+<p class="bg-red-600">
+	Visit <a class="bg-red-600" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
