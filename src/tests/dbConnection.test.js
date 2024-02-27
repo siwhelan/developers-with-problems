fac640a76d19521692ac57a21b5939bb03d59dbf
siwhelan <simon@swhelan.dev>
2024-02-27 12:28:29 +0000

    Broke a test to test the tests

--- src/tests/dbConnection.test.js
+++ src/tests/dbConnection.test.js
@@ -4,6 +4,6 @@ import { connectDB } from '../lib/utils/db';
 describe('Database Connection', () => {
 	it('should use the test database', async () => {
 		const db = await connectDB();
-		expect(db.databaseName).toBe('dwp_test');
+		expect(db.databaseName).toBe('dwp_tes');
 	});
 });
