5909a8ac6c6321e82ad11a38eaf43f86274f1d01
siwhelan <simon@swhelan.dev>
2024-02-27 15:29:26 +0000

    Update import statements in user.js

--- src/lib/models/user.js
+++ src/lib/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-// const bcrypt = require(bcrypt);
+import mongoose from 'mongoose';
+// import bcrypt from 'bcrypt';
 // const hashCost = 12;
 
 const UserSchema = new mongoose.Schema({
