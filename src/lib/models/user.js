8ad8593ebdfbcbc565800d2def19214f1eb095de
siwhelan <simon@swhelan.dev>
2024-02-27 12:22:32 +0000

    ESLINT - Temporarily Commented out hashCost variable in user.js

--- src/lib/models/user.js
+++ src/lib/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require(bcrypt);
-const hashCost = 12;
+// const hashCost = 12;
 
 const UserSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
