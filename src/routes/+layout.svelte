536b40c140a13b0c808f9cbf48679f2cb24d0153
Sophie Earl <sophie.earl@live.com>
2024-02-28 23:50:00 +0000

    saving commit, styling changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,10 @@
 
 <div class="flex">
 	<Navigation />
-	<slot />
+	<div class="flex flex-col flex-grow">
+		<slot />
+	</div>
+	<div class="menu-container bg-blue-100 rounded-lg p-5 m-10 max-w-xs">
+		<p href="/">Placeholder for other sidebar</p>
+	</div>
 </div>
