8ac1d3c4960ee8f3bb061d788357e1767597efb8
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-02-28 17:07:10 +0000

    Working sign up form

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -10,32 +10,28 @@
         // Custom validation logic goes here
         const password = formData.get('password');
         const confirmPassword = formData.get('confirmPassword');
+        console.log(result)
 
         if (password !== confirmPassword) {
-            alert("Passwords do not match");
+            // Passwords don't match, prevent form submission
+            errorMessage ="Passwords do not match";
             cancel();
             return;
         }
 
-        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
-
-        if (!passwordRegex.test(password)) {
-            alert("Password must contain at least 8 characters, a capital letter, a number, and a symbol");
-            cancel();
-            return;
-        }
-        update();
-
+        // If validation passes, you can perform additional actions
+        // ...
 
         // Call the default behavior
-
+        update();
     };
 }}>
-    Us
 	Username: <input type="text" name="username" value="username" required />
 	Email: <input type="email" name="email" value="email" required />
 	Password: <input type="password" name="password" value="password" required />
 	Confirm Password: <input type="password" name="confirmPassword" value="confirmPassword" required />
+
+    {#if errorMessage}<p class="error">{errorMessage}</p>{/if}
 
 	<button type="submit">Sign Up</button>
 </form>
