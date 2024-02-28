daa6e69a9fddfd36e515cbe465ff7917118c0a05
siwhelan <simon@swhelan.dev>
2024-02-28 14:33:56 +0000

    First working version

--- src/routes/events/+page.server.js
+++ src/routes/events/+page.server.js
@@ -1,21 +1,12 @@
-import { connectDB } from '$lib/utils/db';
-import { ObjectId } from 'mongodb'; // Import ObjectId for checking and converting
+// Import the Event model
+import { Event } from '../../lib/models/event.js';
 
-function serializeMongoData(data) {
-	return data.map((event) => ({
-		...event,
-		_id: event._id.toString(), // Convert ObjectId to string
-		title: event.title,
-		startDate: event.startDate.toISOString(), // Convert Date to ISO string
-		endDate: event.endDate.toISOString(), // Convert Date to ISO string
-		attending: event.attending.map((id) => id.toString()), // Convert any ObjectId in arrays
-		interested: event.interested.map((id) => id.toString()) // Convert any ObjectId in arrays
-	}));
-}
-
-export async function load() {
-	const events = [{ title: 'Test Event', startDate: '2024-01-01' }]; // Example hardcoded data
+export const load = async () => {
+	let events = await Event.find().lean();
+	// console.log(events);
+	events = JSON.parse(JSON.stringify(events));
+	events = events.reverse();
 	return {
-		props: { events }
+		events
 	};
-}
+};
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,13 +1,25 @@
 <script>
-	export let events = [];
-	console.log(events);
+	// export let events;
+	export let data;
+	$: ({ events } = data);
 </script>
 
-<div>
-	<h1>Events</h1>
+<div class="pt-3 text-center">
+	<!-- <a
+		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		href="/forum/newEvent">Add an Event</a
+	> -->
+
 	<ul>
 		{#each events as event}
-			<li>{event.title} - {event.startDate}</li>
+			<li key={event._id}>
+				<h2>{event.title}</h2>
+				<p>{event.startDate}</p>
+				<p>{event.location}</p>
+				<p>{event.description}</p>
+				<p>{event.attending}</p>
+				<p>{event.interested}</p>
+			</li>
 		{/each}
 	</ul>
 </div>
