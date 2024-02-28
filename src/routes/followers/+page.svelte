a93ed3a8aa0245d423991d9a28d4130f31b38d7e
AidanJRiley <aidanriley95@gmail.com>
2024-02-28 16:30:22 +0000

    myProfile page added

---
+++ src/routes/followers/+page.svelte
@@ -0,0 +1,11 @@
+<!-- <script> 
+export let followingUserID;
+const handleClick = (userID) => {
+     console.log('trying to follow user');
+    followUser(followingUserID); 
+    }; 
+    </script> 
+    
+<button on:click={handleClick}>
+     Follow 
+    </button> -->
