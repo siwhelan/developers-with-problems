8d9d964ac5f697dcb269f145be9337537b088907
Ed <edward.gemmill@gmail.com>
2024-02-29 15:38:20 +0000

    still no cookies 3

--- src/components/navigation.svelte
+++ src/components/navigation.svelte
@@ -4,19 +4,13 @@
 </script>
 
 <div class="menu-container bg-blue-900 rounded-lg p-5 m-10 max-w-xs">
-<<<<<<< HEAD
-	<a href="/home" class={linkStyles}>Home</a>
-	<a href="/profile" class={linkStyles}>My Profile</a>
-	<a href="/forum" class={linkStyles}>Forum</a>
-	<a href="/events" class={linkStyles}>Events</a>
-	<a href="/jobs" class={linkStyles}>Jobs</a>
-=======
+
 	<a href="/" class={linkStyles}>Home</a>
 	<a href="/profile" class={linkStyles}>My Profile</a>
 	<a href="/forum" class={linkStyles}>Forum</a>
 	<a href="/events" class={linkStyles}>Events</a>
 	<a href="/careers" class={linkStyles}>Careers</a>
->>>>>>> ee917fcb6220942c9d232f1af26da903a461cda7
+
 	<a href="/learning-resources" class={linkStyles}>Learning Resources</a>
 	<a href="/news" class={linkStyles}>News</a>
 	<a href="/settings" class={linkStyles}>Settings</a>
