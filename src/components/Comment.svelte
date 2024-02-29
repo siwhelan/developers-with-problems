a8bfeef25d59a96b50083c8795d40aad97b5907d
Sophie Earl <sophie.earl@live.com>
2024-02-29 12:53:02 +0000

    merging

--- src/components/Comment.svelte
+++ src/components/Comment.svelte
@@ -4,7 +4,7 @@
 	<footer class="flex justify-between items-center mb-2">
 		<div class="flex items-center">
 			<p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-				Helene Engels
+				User3
 			</p>
 			<p class="text-sm text-gray-600 dark:text-gray-400">
 				<time pubdate datetime="2022-06-23" title="June 23rd, 2022">Jun. 23, 2022</time>
