e8717bb1fe80dbae61847c14fadb916086988cfc
siwhelan <simon@swhelan.dev>
2024-02-29 12:13:15 +0000

    Refactor job loading and add unit tests

--- src/routes/careers/+page.server.js
+++ src/routes/careers/+page.server.js
@@ -4,7 +4,6 @@ export const load = async () => {
 	let jobs = await Job.find().lean();
 	// console.log(jobs);
 	jobs = JSON.parse(JSON.stringify(jobs));
-	jobs = jobs.reverse();
 	return {
 		jobs
 	};
