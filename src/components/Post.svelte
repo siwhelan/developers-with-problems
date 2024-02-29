acb7ac4bfcb610b244ca55c5881200cd9543bb4e
Sophie Earl <sophie.earl@live.com>
2024-02-29 15:50:47 +0000

    adds upvote svgs

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,12 +1,32 @@
 <script>
+	import Upvote from './upvote.svelte';
+	import Upvoted from './upvoted.svelte';
+
 	export let postTitle;
 	export let postContent;
 	export let postAuthor;
+	export let upvoteNumber = 1;
+
+	export let userUpvoted = true;
 </script>
 
 <div class="flex flex-row py-5 pr-28 bg-slate-100">
 	<div>
-		<p class="text-xl py-2 px-3">5 ⬆️</p>
+		{#if !userUpvoted}
+			<div class="flex">
+				<div class="max-h-1">
+					<Upvote />
+				</div>
+				<p class="text-xl py-2">{upvoteNumber}</p>
+			</div>
+		{:else if userUpvoted}
+			<div class="flex">
+				<div class="max-h-1">
+					<Upvoted />
+				</div>
+				<p class="text-xl py-2">{upvoteNumber}</p>
+			</div>
+		{/if}
 		<p class="text-xl py-2 px-3">1 💬</p>
 	</div>
 	<!-- upvote and comment icon to be added -->
