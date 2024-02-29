e6455043b9dae5dfe74c7fd71dd84a7e6a6662fc
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-02-29 11:51:31 +0000

    format

--- src/components/UserNavBar.svelte
+++ src/components/UserNavBar.svelte
@@ -1,29 +1,29 @@
 <script>
-    import Page from "../routes/signup/+page.svelte";
+	import Page from '../routes/signup/+page.svelte';
 
-    let session = false;
+	let session = false;
 </script>
 
 <div class="bg-gray-800 text-white p-4">
-    <nav class="flex justify-between">
-        <ul class="flex space-x-4 items-center">
-            <li>
-                <a href="/home">
-                    <img src="/dwp.png" class="hover:underline w-25 h-8">
-                </a>
-            </li>
-        </ul>
-        <ul class="flex space-x-4">
-            <li>
-                {#if session}<a href="/profile" class="hover:underline">Profile</a>{/if}
-            </li>
-            <li>
-                {#if !session}<a href="/signup" class="hover:underline">Sign Up</a>{/if}      
-            </li> 
-            <li>
-                {#if !session}<a href="/login" class="hover:underline">Login</a>
-                {:else}<a href="/logout" class="hover:underline">Logout</a>{/if}
-            </li>
-        </ul>
-    </nav>
+	<nav class="flex justify-between">
+		<ul class="flex space-x-4 items-center">
+			<li>
+				<a href="/home">
+					<img src="/dwp.png" class="hover:underline w-25 h-8" />
+				</a>
+			</li>
+		</ul>
+		<ul class="flex space-x-4">
+			<li>
+				{#if session}<a href="/profile" class="hover:underline">Profile</a>{/if}
+			</li>
+			<li>
+				{#if !session}<a href="/signup" class="hover:underline">Sign Up</a>{/if}
+			</li>
+			<li>
+				{#if !session}<a href="/login" class="hover:underline">Login</a>
+				{:else}<a href="/logout" class="hover:underline">Logout</a>{/if}
+			</li>
+		</ul>
+	</nav>
 </div>
