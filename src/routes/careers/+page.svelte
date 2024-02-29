2eecd9040b6e5b8af49cc82d352de428bff6a8af
siwhelan <simon@swhelan.dev>
2024-02-29 10:45:12 +0000

    Refactor careers page to display job details

--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -1,20 +1,34 @@
 <script>
 	// export let posts;
 	export let data;
-	$: ({ posts } = data);
+	$: ({ jobs } = data);
+
+	// function to format the date
+	function formatDate(dateString) {
+		const options = { year: 'numeric', month: 'long', day: 'numeric' };
+		return new Date(dateString).toLocaleDateString(undefined, options);
+	}
 </script>
 
 <div class="pt-3 text-center">
-	<a
+	<!-- <a
 		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 		href="/forum/newPost">Make a new post</a
-	>
+	> -->
 
 	<ul>
-		{#each posts as post}
-			<li key={post._id}>
-				<h2>{post.title}</h2>
-				<p>{post.content}</p>
+		{#each jobs as job}
+			<li key={job._id}>
+				<h2>{job.title}</h2>
+				<p>{job.description}</p>
+				<p>{job.level}</p>
+				<p>{job.salary}</p>
+				<p>{job.company}</p>
+				<p>{job.location}</p>
+				<p>{job.tags}</p>
+				<p>{formatDate(job.timestamp)}</p>
+				<p>{formatDate(job.closingDate)}</p>
+				<p>{job.userId}</p>
 			</li>
 		{/each}
 	</ul>
