438ead0eedb66415fc65b0ce67af89b6ef310544
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-02-29 11:28:38 +0000

    Completed user navbar component

---
+++ src/components/UserNavBar.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Page from "../routes/signup/+page.svelte";
+
+    let session = false;
+</script>
+
+<div class="bg-gray-800 text-white p-4">
+    <nav class="flex justify-between">
+        <ul class="flex space-x-4 items-center"> <!-- Added items-center here -->
+            <li>
+                <a href="/home">
+                    <img src="/dwp.png" class="hover:underline w-25 h-8"> <!-- Adjusted width and height here -->
+                </a>
+            </li>
+        </ul>
+        <ul class="flex space-x-4">
+            <li>
+                {#if session}<a href="/profile" class="hover:underline">Profile</a>{/if}
+            </li>
+            <li>
+                {#if !session}<a href="/signup" class="hover:underline">Sign Up</a>{/if}      
+            </li> 
+            <li>
+                {#if !session}<a href="/login" class="hover:underline">Login</a>
+                {:else}<a href="/logout" class="hover:underline">Logout</a>{/if}
+            </li>
+        </ul>
+    </nav>
+</div>
