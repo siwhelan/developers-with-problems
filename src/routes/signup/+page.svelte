ce112592ee9426beb7ee51d44f33d8cd3de9613d
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-03-01 12:03:31 +0000

    changed sign up button

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -99,7 +99,7 @@
 			<button
 				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
 				type="submit"
-				>Log In</button
+				>Sign Up</button
 			>
 	</form>
 	{/if}
