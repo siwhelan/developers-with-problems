1063f82e3a822eeda0c1204220aed44b7ca08efa
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-03-01 16:43:01 +0000

    formatting

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,30 @@
 <div class="pt-3 text-center">
-<h1 class="text-2xl font-bold mb-4">Log In To DWP</h1>
-<form class="max-w-md mx-auto p-4 bg-white shadow-md" method="POST" action="?/authorisation">
-	<div class="mb-4">
-	<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email:</label>
-	<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="email" value="" placeholder="email" required />
-	</div>
-	<div class="mb-4">
-	<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Password:</label>
-	<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" name="password" value="" placeholder="password" required />
-	</div>
-	<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4" type="submit">Login</button>
-</form>
+	<h1 class="text-2xl font-bold mb-4">Log In To DWP</h1>
+	<form class="max-w-md mx-auto p-4 bg-white shadow-md" method="POST" action="?/authorisation">
+		<div class="mb-4">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email:</label>
+			<input
+				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+				type="text"
+				name="email"
+				value=""
+				placeholder="email"
+				required
+			/>
+		</div>
+		<div class="mb-4">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Password:</label>
+			<input
+				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+				type="password"
+				name="password"
+				value=""
+				placeholder="password"
+				required
+			/>
+		</div>
+		<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4" type="submit"
+			>Login</button
+		>
+	</form>
 </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -11,97 +11,96 @@
 
 <div class="pt-3 text-center">
 	{#if success}
-	<SignUpSuccess />
+		<SignUpSuccess />
 	{:else}
-	<h1 class="text-2xl font-bold mb-4">Sign Up To DWP</h1>
-	<form
-		class="max-w-md mx-auto p-4 bg-white shadow-md"
-		method="POST"
-		action="?/create"
-		use:enhance={({ formData, cancel }) => {
-			return async ({ result, update }) => {
-				const password = formData.get('password');
-				const confirmPassword = formData.get('confirmPassword');
+		<h1 class="text-2xl font-bold mb-4">Sign Up To DWP</h1>
+		<form
+			class="max-w-md mx-auto p-4 bg-white shadow-md"
+			method="POST"
+			action="?/create"
+			use:enhance={({ formData, cancel }) => {
+				return async ({ result, update }) => {
+					const password = formData.get('password');
+					const confirmPassword = formData.get('confirmPassword');
 
-				if (password !== confirmPassword) {
-					errorMessage = 'Passwords do not match';
-					cancel();
-					return;
-				}
+					if (password !== confirmPassword) {
+						errorMessage = 'Passwords do not match';
+						cancel();
+						return;
+					}
 
-				if (result.data.success == true) {
-					success = true;
-				}
+					if (result.data.success == true) {
+						success = true;
+					}
 
-				errorMessage = result.data.error;
-				update();
-			};
-		}}
-	>
-		<div class="mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username:</label>
-			<input
-				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				type="text"
-				name="username"
-				id="username"
-				value=""
-				placeholder="username"
-				required
-				on:focus={clearErrorMessage}
-			/>
-		</div>
+					errorMessage = result.data.error;
+					update();
+				};
+			}}
+		>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username:</label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					type="text"
+					name="username"
+					id="username"
+					value=""
+					placeholder="username"
+					required
+					on:focus={clearErrorMessage}
+				/>
+			</div>
 
-		<div class="mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email:</label>
-			<input
-				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				type="email"
-				name="email"
-				id="email"
-				value=""
-				placeholder="email"
-				required
-				on:focus={clearErrorMessage}
-			/>
-		</div>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email:</label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					type="email"
+					name="email"
+					id="email"
+					value=""
+					placeholder="email"
+					required
+					on:focus={clearErrorMessage}
+				/>
+			</div>
 
-		<div class="mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password:</label>
-			<input
-				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				type="password"
-				name="password"
-				id="password"
-				value=""
-				placeholder="password"
-				required
-				on:focus={clearErrorMessage}
-			/>
-		</div>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password:</label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					type="password"
+					name="password"
+					id="password"
+					value=""
+					placeholder="password"
+					required
+					on:focus={clearErrorMessage}
+				/>
+			</div>
 
-		<div class="mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="confirmPassword"
-				>Confirm Password:</label
-			>
-			<input
-				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				type="password"
-				name="confirmPassword"
-				id="confirmPassword"
-				value=""
-				placeholder="password"
-				required
-				on:focus={clearErrorMessage}
-			/>
-		</div>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="confirmPassword"
+					>Confirm Password:</label
+				>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					type="password"
+					name="confirmPassword"
+					id="confirmPassword"
+					value=""
+					placeholder="password"
+					required
+					on:focus={clearErrorMessage}
+				/>
+			</div>
 
-		{#if errorMessage}<p class="text-red-500">{errorMessage}</p>{/if}
+			{#if errorMessage}<p class="text-red-500">{errorMessage}</p>{/if}
 			<button
 				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-				type="submit"
-				>Sign Up</button
+				type="submit">Sign Up</button
 			>
-	</form>
+		</form>
 	{/if}
 </div>
