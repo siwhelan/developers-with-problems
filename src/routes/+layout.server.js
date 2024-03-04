af07de2e91a200d18af51541b55ddf1de26a67e1
AidanJRiley <aidanriley95@gmail.com>
2024-03-04 15:09:50 +0000

    fixed issues with being logged out

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,14 +1,30 @@
 import { User } from '../lib/models/user.js';
 
 export const load = async ({ locals }) => {
-	console.log('-----------------');
-	console.log(locals);
-	const currentUserID = locals.user.id;
-	// const currentUserID = '123';
-	const currentUser = await User.findOne({ _id: currentUserID });
-	console.log(currentUser);
-	const currentUserUsername = currentUser.username;
-	console.log(currentUserUsername);
+	let currentUserUsername;
+	try {
+		if (locals.user) {
+			const currentUserID = locals.user.id;
+			console.log(currentUserID);
+			// const currentUserID = '123';
+			const currentUser = await User.findOne({ _id: currentUserID });
+			console.log(currentUser);
+			if (currentUser) {
+				currentUserUsername = currentUser.username;
+				console.log(currentUserUsername);
+			} else {
+				console.error('User not found in database');
+				currentUserUsername = null;
+			}
+		} else {
+			console.error('No user found in locals');
+			currentUserUsername = null;
+		}
+	} catch (error) {
+		console.error('Error getting logged in user:', error);
+		currentUserUsername = null;
+	}
+
 	return {
 		currentUserUsername
 	};
