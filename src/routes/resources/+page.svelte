72088f665126e3365f512bd32af524f7b64e50cd
Sophie Earl <sophie.earl@live.com>
2024-03-04 17:25:34 +0000

    basic resource page

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -2,6 +2,17 @@
 	import resources from './resources.json';
 </script>
 
-{#each resources as resource}
-	<p>{resource.name}</p>
-{/each}
+<div class="pt-24">
+	<h2 class="text-3xl font-bold pb-2">Learning Resources</h2>
+	<h3>Resources recommended by us.</h3>
+</div>
+<ul class="list-inside mt-4">
+	{#each resources as resource}
+		<li class="mt-2">
+			<a href={resource.url} target="_blank" class="text-text hover:text-primary"
+				>🔗 {resource.name}</a
+			>
+			<p class="pb-5">-- {resource.description}</p>
+		</li>
+	{/each}
+</ul>
