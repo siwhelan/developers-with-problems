ee0b4b6116de576c20f824eb6ead58dcd6d955a6
Sophie Earl <sophie.earl@live.com>
2024-03-05 18:30:30 +0000

    styling fixed

--- src/components/Comment.svelte
+++ src/components/Comment.svelte
@@ -95,7 +95,7 @@
 </script>
 
 <article
-	class="p-6 text-base bg-background shadow-sm border-t border-gray-200 dark:border-gray-700 dark:bg-gray-800"
+	class="p-6 text-base bg-background shadow-sm border-t flex flex-row border-gray-200 dark:border-gray-700 dark:bg-gray-800"
 >
 	<div class="mr-1">
 		<div class="w-20 h-14">
@@ -147,70 +147,74 @@
 			{/if}
 		</div>
 	</div>
-	<div class="flex justify-between items-center mb-2">
-		<div class="flex items-center">
-			<p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-				{commentUser}
-			</p>
-			<p class="text-sm text-gray-600 dark:text-gray-400">
-				<time pubdate datetime="2022-06-23" title="June 23rd, 2022">{timeAgo(commentTime)}</time>
-			</p>
-		</div>
-		<button
-			id="dropdownComment4Button"
-			data-dropdown-toggle="dropdownComment4"
-			aria-label="edit comment"
-			class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-800 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-			type="button"
-		>
-			<svg
-				class="w-4 h-4"
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="currentColor"
-				viewBox="0 0 16 3"
+	<div class="flex flex-col flex-grow">
+		<div class="flex justify-between mb-2">
+			<div class="flex items-center">
+				<p
+					class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"
+				>
+					{commentUser}
+				</p>
+				<p class="text-sm text-gray-600 dark:text-gray-400">
+					<time pubdate datetime="2022-06-23" title="June 23rd, 2022">{timeAgo(commentTime)}</time>
+				</p>
+			</div>
+			<button
+				id="dropdownComment4Button"
+				data-dropdown-toggle="dropdownComment4"
+				aria-label="edit comment"
+				class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-800 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				type="button"
 			>
-				<path
-					d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"
-				/>
-			</svg>
-		</button>
-		<!-- Dropdown menu -->
-		<div
-			id="dropdownComment4"
-			class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-		>
-			<ul
-				class="py-1 text-sm text-gray-800 dark:text-gray-200"
-				aria-labelledby="dropdownMenuIconHorizontalButton"
+				<svg
+					class="w-4 h-4"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="currentColor"
+					viewBox="0 0 16 3"
+				>
+					<path
+						d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"
+					/>
+				</svg>
+			</button>
+			<!-- Dropdown menu -->
+			<div
+				id="dropdownComment4"
+				class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
 			>
-				<li>
-					<a
-						href="edit"
-						class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-						>Edit</a
-					>
-				</li>
-				<li>
-					<a
-						href="remove"
-						class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-						>Remove</a
-					>
-				</li>
-				<li>
-					<a
-						href="report"
-						class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-						>Report</a
-					>
-				</li>
-			</ul>
+				<ul
+					class="py-1 text-sm text-gray-800 dark:text-gray-200"
+					aria-labelledby="dropdownMenuIconHorizontalButton"
+				>
+					<li>
+						<a
+							href="edit"
+							class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Edit</a
+						>
+					</li>
+					<li>
+						<a
+							href="remove"
+							class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Remove</a
+						>
+					</li>
+					<li>
+						<a
+							href="report"
+							class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Report</a
+						>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div>
 
-	<p class="text-gray-800 dark:text-gray-400">
-		{commentContent}
-	</p>
-	<div class="flex items-center mt-4 space-x-4"></div>
+		<p class="text-gray-800 dark:text-gray-400">
+			{commentContent}
+		</p>
+		<div class="flex items-center mt-4 space-x-4"></div>
+	</div>
 </article>
