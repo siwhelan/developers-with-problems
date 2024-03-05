4032e8457018e001e15fefe5138829d49514bff2
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-03-05 12:32:40 +0000

    Highlight white

--- src/components/CodewarsInfo.svelte
+++ src/components/CodewarsInfo.svelte
@@ -24,7 +24,7 @@
 				<div class="absolute top-0 left-0 w-full h-[102%]"></div>
 			</div>
 			<div
-				class="pl-2 group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
+				class="pl-2 group-hover:bg-white/10 hover:text-white transition duration-600 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
 				href="#"
 			>
 				<!-- <svg class="h-6 w-6 group-hover:fill-red-600 dark:fill-gray-600 transition-colors duration-200 pt-1 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 768 768"><path fill="#ee852f" d="M400 0H176c-26.5 0-48.1 21.8-47.1 48.2c.2 5.3 .4 10.6 .7 15.8H24C10.7 64 0 74.7 0 88c0 92.6 33.5 157 78.5 200.7c44.3 43.1 98.3 64.8 138.1 75.8c23.4 6.5 39.4 26 39.4 45.6c0 20.9-17 37.9-37.9 37.9H192c-17.7 0-32 14.3-32 32s14.3 32 32 32H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H357.9C337 448 320 431 320 410.1c0-19.6 15.9-39.2 39.4-45.6c39.9-11 93.9-32.7 138.2-75.8C542.5 245 576 180.6 576 88c0-13.3-10.7-24-24-24H446.4c.3-5.2 .5-10.4 .7-15.8C448.1 21.8 426.5 0 400 0zM48.9 112h84.4c9.1 90.1 29.2 150.3 51.9 190.6c-24.9-11-50.8-26.5-73.2-48.3c-32-31.1-58-76-63-142.3zM464.1 254.3c-22.4 21.8-48.3 37.3-73.2 48.3c22.7-40.3 42.8-100.5 51.9-190.6h84.4c-5.1 66.3-31.1 111.2-63 142.3z"/></svg> -->
@@ -48,7 +48,7 @@
 				<div class="absolute top-0 left-0 w-full h-[102%] translate-y-full"></div>
 			</div>
 			<div
-				class="pl-2 group-hover:bg-white/10 w-full group-active:scale-95 self-stretch rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
+				class="pl-2 group-hover:bg-white/10 hover:text-white transition duration-600 w-full group-active:scale-95 self-stretch rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
 				href="#"
 			>
 				<!-- <svg class="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600 transition-colors duration-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -74,7 +74,7 @@
 				<div class="absolute top-0 left-0 w-full h-[102%] translate-y-full"></div>
 			</div>
 			<div
-				class="pl-2 group-hover:bg-white/10 w-full group-active:scale-95 self-stretch rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
+				class="pl-2 group-hover:bg-white/10 hover:text-white transition duration-100 w-full group-active:scale-95 self-stretch rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
 				href="#"
 			>
 				<!-- <svg class="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -101,7 +101,7 @@
 				<div class="absolute top-0 left-0 w-full h-[102%] translate-y-full"></div>
 			</div>
 			<div
-				class="pl-2 group-hover:bg-white/10 w-full group-active:scale-95 self-stretch rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
+				class="pl-2 group-hover:bg-white/10 hover:text-white transition duration-600 w-full group-active:scale-95 self-stretch rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
 				href="#"
 			>
 				<!-- <svg class="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
