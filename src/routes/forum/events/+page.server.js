80061809ce62a6849b6c52aa91b7d02e3964cb4e
Sophie Earl <sophie.earl@live.com>
2024-03-05 13:14:39 +0000

    fixes upvotes

--- src/routes/forum/events/+page.server.js
+++ src/routes/forum/events/+page.server.js
@@ -28,3 +28,16 @@ export async function load({ locals }) {
 		loggedInUser
 	};
 }
+export const actions = {
+	upvote: async ({ locals, request }) => {
+		const { action, postSlug } = await request.json();
+		let post = await Post.findById(postSlug);
+		let loggedInUser = locals.user.id;
+		if (action == false) {
+			post.upvotes.push(loggedInUser);
+		} else {
+			post.upvotes = post.upvotes.filter((like) => like.toString() !== loggedInUser);
+		}
+		await post.save();
+	}
+};
