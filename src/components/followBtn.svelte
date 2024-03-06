c2af5f1a6af20df9f4854c9f5b4f63065f5f9a00
Sophie Earl <sophie.earl@live.com>
2024-03-06 13:21:19 +0000

    changes

--- src/components/followBtn.svelte
+++ src/components/followBtn.svelte
@@ -9,7 +9,7 @@
 	{#if isFollowing}
 		<button
 			aria-label="unfollow"
-			class="block py-2 px-4 bg-green-800 text-white rounded-md mb-2 max-w-xs hover:bg-red-700 text-center"
+			class="py-2 px-4 bg-green-800 text-white rounded-md mb-2 hover:bg-red-700 text-center"
 			on:click={() => onClick(profileUserID, isFollowing)}
 		>
 			Unfollow
@@ -17,7 +17,7 @@
 	{:else}
 		<button
 			aria-label="follow"
-			class="block py-2 px-4 bg-green-800 text-white rounded-md mb-2 max-w-xs hover:bg-red-700 text-center"
+			class="py-2 px-4 bg-green-800 text-white rounded-md mb-2 hover:bg-red-700 text-center"
 			on:click={() => onClick(profileUserID, isFollowing)}
 		>
 			Follow
